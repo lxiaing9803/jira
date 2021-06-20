54aab96112c4092ae117aaa8ae13553108e481dd
ashinLX <1014027506@qq.com>
2021-06-21 00:01:05 +0800

    feat: 添加json-server和工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { SearchPanel } from './search-panel';
+import { List } from './list';
+import { cleanObject } from 'utils';
+import qs from 'qs';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: '',
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const SearchPanel = ({ users, param, setParam }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(e) =>
+            setParam({
+              ...param,
+              name: e.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(e) =>
+            setParam({
+              ...param,
+              personId: e.target.value,
+            })
+          }
+        >
+          <option value="">负责人</option>
+          {users.map((user) => {
+            return (
+              <option value={user.id} key={user.id}>
+                {user.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </form>
+  );
+};
