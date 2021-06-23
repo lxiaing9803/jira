cc19c343e3366c337b6571bd473384a2cf050ab8
ashinLX <1014027506@qq.com>
2021-06-23 22:56:07 +0800

    chore: 暂存文件以安装jira-tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-// import {ProjectListScreen} from 'screens/project-list';
-import { TsReactTest } from 'try-use-array'
+// import { ProjectListScreen } from 'screens/project-list';
+import {LoginScreen} from 'screens/login'
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest />
       {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,44 @@
+import React, { FormEvent } from 'react';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+
+  const login = (param:{username:string,password:string})=>{
+    fetch(`${apiUrl}/login`,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(param)
+    }).then(
+      async(response)=>{
+        if(response.ok){
+
+        }
+      }
+    )
+  }
+
+  const handleSubmit = (event:FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    // 不写as HTMLInputElement的话就会把event.currentTarget.elements[0]当做一个element类型
+    const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+    login({username,password})
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="userName">用户名</label>
+        <input type="text" id="userName" />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id="password" />
+      </div>
+      <button type="submit">登录</button>
+    </form>
+  );
+};
