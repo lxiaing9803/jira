4c4e531c3e9cd5609b03ebaf445e6fa970915bce
ashinLX <1014027506@qq.com>
2021-06-23 23:04:33 +0800

    chore: 安装jira-tool和删除json-server

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {loadDevTools} from 'jira-dev-tool'
 
-ReactDOM.render(
+loadDevTools(()=>ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
