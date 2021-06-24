5238b86637d26a3fa9faa380d81a5c3e14c23167
ashinLX <1014027506@qq.com>
2021-06-24 22:37:13 +0800

    feat: 添加登录注册

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { AuthenticatedApp } from 'authenticated-app';
+import { useAuth } from 'context/auth-context';
 import React from 'react';
+import { UnauthenticatedApp } from 'unauthenticated-app';
 import './App.css';
-// import { ProjectListScreen } from 'screens/project-list';
-import {LoginScreen} from 'screens/login'
 
 function App() {
+  const { user } = useAuth()
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
---
+++ src/unauthenticated-app/register.tsx
@@ -0,0 +1,31 @@
+import { useAuth } from 'context/auth-context';
+import React, { FormEvent } from 'react';
+
+
+
+export const RegisterScreen = () => {
+
+    const { register } = useAuth()
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // 不写as HTMLInputElement的话就会把event.currentTarget.elements[0]当做一个element类型
+        const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+        const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+        register({ username, password })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="userName">用户名</label>
+                <input type="text" id="userName" />
+            </div>
+            <div>
+                <label htmlFor="password">密码</label>
+                <input type="password" id="password" />
+            </div>
+            <button type="submit">注册</button>
+        </form>
+    );
+};
