f233e4e96a081328d31c122126fc42f68888a483
ashinLX <1014027506@qq.com>
2021-06-29 23:38:14 +0800

    feat: 添加ErrorBoundray

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { AuthenticatedApp } from 'authenticated-app';
+import { ErrorBoundary } from 'components/error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 import { useAuth } from 'context/auth-context';
 import { UnauthenticatedApp } from 'unauthenticated-app';
 import './App.css';
@@ -7,7 +9,9 @@ function App() {
   const { user } = useAuth()
   return (
     <div className="App">
-      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      <ErrorBoundary fallbackRender={FullPageErrorFallback} >
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
     </div>
   );
 }
